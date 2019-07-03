45c7b6fee4779867bc835cef1258b27b27733162
Aude Faucheux <aude53@gmail.com>
2019-07-03 22:10:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function takeANumber(currentLine, personName) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length !== 0) {
-    return `Currently serving ${katzDeliLine.slice(0,1)}.`
+    var serving = katzDeliLine.slice(0,1)
     katzDeliLine.shift()
+    return `Currently serving ${serving}.`
   } else {return "There is nobody waiting to be served!"}
 }
