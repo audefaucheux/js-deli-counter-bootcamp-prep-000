44a3de3197e62e1da7eb81d1f25ded8a9a3c5f86
Aude Faucheux <aude53@gmail.com>
2019-07-03 21:30:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function takeANumber(currentLine, personName) {
-  return `Welcome ${personName.slice(currentLine,currentLine + 1)}. You are number ${currentLine + 1} in line.`
+  return `Welcome ${personName.slice(0,1)}. You are number 1 in line.`
 }
