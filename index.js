b207fa8f9c8b2af19f71d7bb475bf01a5584dc88
Aude Faucheux <aude53@gmail.com>
2019-07-03 22:51:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function currentLine(line) {
         waiting += `${i + 1}. ${line.slice(i, i + 1)}, `
       } else {waiting += `${i + 1}. ${line.slice(i, i + 1)}`}
     }
-  return waiting.slice(0, -1);
+  return waiting.slice
   } else { return "The line is currently empty."}
 }
