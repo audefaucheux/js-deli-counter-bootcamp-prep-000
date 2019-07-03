9515605ef3998403340c155dd3485a6924c44ab0
Aude Faucheux <aude53@gmail.com>
2019-07-03 21:43:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 function takeANumber(currentLine, personName) {
-  var i = 0
-  currentLine.push(i + 1)
-  return `Welcome, ${personName}. You are number ${currentLine.slice(i, i+1)} in line.`
+  return `Welcome, ${personName}. You are number ${currentLine.length + 1} in line.`
 }
